3e82860aaf7ddcaf3e512adafa6956bee23c8d77
grospitz-wreck-it <grospitz@gmail.com>
2026-09-02 23:08:01 +0200

    feat(engine): export drift state

--- packages/engine-core/src/index.ts
+++ packages/engine-core/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./eventBus";
 export * from "./feedEngine";
+export * from "./driftState";
 export * from "./realtimeEventBridge";
 export * from "./recordInteraction";
 export * from "./analyticsEngine";
